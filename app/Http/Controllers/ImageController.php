f932713c8ff15aadf877c978357960de636f3b6d
lbzuluagag <lbzuluagag@eafit.edu.co>
2021-05-18 02:40:49 -0500

    parece que esta funcionando esto

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -4,9 +4,31 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Image;
+
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Storage;
 class ImageController extends Controller
 {
-    
+    public static $quotes = array(
+        "quote/quote1.jpg",
+        "quote/quote2.jpg",
+        "quote/quote3.jpg",
+        "quote/quote4.jpg",
+        "quote/quote5.jpg",
+        "quote/quote6.jpg",
+        "quote/quote7.jpg",
+        "quote/quote8.jpg",
+        "quote/quote9.jpg",
+        "quote/quote10.jpg",
+        "quote/quote11.jpg",
+        "quote/quote12.jpg",
+        "quote/quote13.jpg",
+        "quote/quote14.jpg",
+        "quote/quote15.jpg",
+        
+
+
+);
 
     public function create()
     {
@@ -19,8 +41,15 @@ class ImageController extends Controller
 
         return $path;
     }
-    public function show(Image $image)
+    
+    public function show()
     {
-
+        $data = [];
+        $totalQuotes = (count(ImageController::$quotes));
+        $randomNumber = (rand(0,($totalQuotes-1)));
+        $url = ImageController::$quotes[$randomNumber];
+        $data['image'] = $url;
+        $data['ip'] = gethostbyname(gethostname());
+        return view('images.create')->with("data",$data);
     }
 }
